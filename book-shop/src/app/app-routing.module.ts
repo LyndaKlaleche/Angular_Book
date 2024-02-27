1db8231799ad4b179c4e7d3536780214bd24fe1d
LyndaKlaleche <lyndaklaleche2020@gmail.com>
2024-02-27 16:58:42 +0100

    Ajout des routes

--- book-shop/src/app/app-routing.module.ts
+++ book-shop/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ListBooksComponent } from './list-books/list-books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :"Edit", component : EditBookComponent},
+  {path :"Add", component : AddBookComponent},
+  {path :"List", component : ListBooksComponent}, 
+  {path :"", redirectTo: "List", pathMatch :'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- book-shop/src/app/app.module.ts
+++ book-shop/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ListBooksComponent } from './list-books/list-books.component';
 import { DetailsBookComponent } from './details-book/details-book.component';
 import { SearchBookComponent } from './search-book/search-book.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListBooksComponent,
     DetailsBookComponent,
-    SearchBookComponent
+    SearchBookComponent,
+    AddBookComponent,
+    EditBookComponent
   ],
   imports: [
     BrowserModule,
